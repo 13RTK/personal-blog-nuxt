b115f95e7349db386edb7a5e4499ce567ff69c98
alex <907785012@qq.com>
2025-08-18 02:37:54 +0800

    complete config for cloudflare pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,13 @@ import tailwindcss from '@tailwindcss/vite';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
   },
   modules: ['motion-v/nuxt', 'nuxt-toast'],
+  nitro: {
+    preset: 'cloudflare-pages',
+  },
 });
